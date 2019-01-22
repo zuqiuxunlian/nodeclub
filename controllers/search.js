d76cb2a9567472ebd4f6de71d3a5b257a100bae4
Ten Wong <wangtengoo7@gmail.com>
2019-01-22 10:48:54 +0800

    no message

--- controllers/search.js
+++ controllers/search.js
@@ -1,5 +1,6 @@
 exports.index = function (req, res, next) {
   var q = req.query.q;
   q = encodeURIComponent(q);
-  res.redirect('https://www.google.com.hk/#hl=zh-CN&q=site:cnodejs.org+' + q);
+  // res.redirect('https://www.google.com.hk/#hl=zh-CN&q=site:bbs.zuqiuxunlian.com+' + q);
+  res.redirect('https://www.baidu.com/s?wd=site:bbs.zuqiuxunlian.com ' + q);
 };
