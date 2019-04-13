a02d259f1fb31c6b0a204e678bd75301c710b74d
lee <274153705@qq.com>
2019-04-13 19:55:32 +0800

    fix: reset code

--- proxy/user.js
+++ proxy/user.js
@@ -31,6 +31,18 @@ exports.getUserByOpenid = function (openid, callback) {
 };
 
 /**
+ * 根据登录名查找用户
+ * Callback:
+ * - err, 数据库异常
+ * - user, 用户
+ * @param {String} loginName 登录名
+ * @param {Function} callback 回调函数
+ */
+exports.getUserByLoginName = function (loginName, callback) {
+  User.findOne({'loginname': new RegExp('^'+loginName+'$', "i")}, callback);
+};
+
+/**
  * 根据用户ID，查找用户
  * Callback:
  * - err, 数据库异常
