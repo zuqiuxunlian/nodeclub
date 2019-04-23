9634c2a1f3422ca058c500321b9889d67080e719
Ten Wong <wangtengoo7@gmail.com>
2019-04-24 00:44:25 +0800

    输出nickname

--- api/v1/topic.js
+++ api/v1/topic.js
@@ -42,7 +42,7 @@ var index = function (req, res, next) {
         if (mdrender) {
           topic.content = renderHelper.markdown(at.linkUsers(topic.content));
         }
-        topic.author = _.pick(author, ['loginname', 'avatar_url']);
+        topic.author = _.pick(author, ['loginname', 'name', 'avatar_url']);
         ep.emit('author');
       }));
     });
@@ -88,13 +88,13 @@ var show = function (req, res, next) {
     if (mdrender) {
       topic.content = renderHelper.markdown(at.linkUsers(topic.content));
     }
-    topic.author = _.pick(author, ['loginname', 'avatar_url']);
+    topic.author = _.pick(author, ['loginname', 'name', 'avatar_url']);
 
     topic.replies = replies.map(function (reply) {
       if (mdrender) {
         reply.content = renderHelper.markdown(at.linkUsers(reply.content));
       }
-      reply.author = _.pick(reply.author, ['loginname', 'avatar_url']);
+      reply.author = _.pick(reply.author, ['loginname', 'name', 'avatar_url']);
       reply =  _.pick(reply, ['id', 'author', 'content', 'ups', 'create_at', 'reply_id']);
       reply.reply_id = reply.reply_id || null;
 
